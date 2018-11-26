e2fe823bbc5e034d4df9390879e5d9c90ae6b783
gomezcalderondaniel <42280710+gomezcalderondaniel@users.noreply.github.com>
2018-11-25 22:44:26 -0700

    add files via upload

--- main.js
+++ main.js
@@ -16,6 +16,13 @@ const CLIENT_ID = "1037635597104-3onij1kaerr43diim6kfu579e5qtnod6.apps.googleuse
  const defaultChannel = "techguyweb";
 
 
+
+channelForm.addEventListener("submit", e =>{
+    e.preventDefault();
+    const channel = channelInput.value;
+    getChannel(channel);
+});
+
  //Load auth2 Library
  function handleClientLoad(){
      gapi.load("client:auth2", initClient)
@@ -29,9 +36,9 @@ const CLIENT_ID = "1037635597104-3onij1kaerr43diim6kfu579e5qtnod6.apps.googleuse
          scope: SCOPES
      }).then(() =>{
         //LIsten for sign in state changes
-        gapi.auth2.getAuthInstant().isSignedIn.listen(updateSigninStatus);
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
         //Handle Initial Sign in State
-        updateSigninStatus(gapi.auth2.getAuthInstant().isSignedIn.get());
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
         authorizeButton.onclick = handleAuthClick;
         signoutButton.onclick = handleSignoutClick;
      });
@@ -51,21 +58,99 @@ const CLIENT_ID = "1037635597104-3onij1kaerr43diim6kfu579e5qtnod6.apps.googleuse
         content.style.display = "none";
         videoContainer.style.display = "none";
      }
-     
+      
  }
 
  //Handle Login
  function handleAuthClick(){
-     gapi.auth2.getAuthInstant().signIn();
+     gapi.auth2.getAuthInstance().signIn();
 
  }
 
  //Handle Sing out
  function handleSignoutClick(){
-     gapi.auth2.getAuthInstant().signOut();
+     gapi.auth2.getAuthInstance().signOut();
  }
+
+//Display Channel Data
+function showChannelData(data){
+    const channelData = document.getElementById("channel-data");
+    channelData.innerHTML = data;
+}
+
 
  //Get channel from API
  function getChannel(channel){
      console.log(channel);
+     gapi.client.youtube.channels.list({
+        part: "snippet, contentDetails, statistics",
+        forUsername: channel
+
+     }).then(response => {
+         console.log(response);
+         const channel = response.result.items[0];
+         const output = `
+         <ul class = "collection">
+            <li class="collection-item">Tile: ${channel.snippet.tile}</li>
+            <li class="collection-item">ID: ${channel.id}</li>
+            <li class="collection-item">Subscribers: ${numWithCommas(channel.statistics.subscriberCount)}</li>
+            <li class="collection-item">Views: ${numWithCommas(channel.statistics.viewCount)}</li>
+            <li class="collection-item">Videos: ${numWithCommas(channel.statistics.videoCount)}</li>
+         </ul>
+         <p>${channel.snippet.description}</p>
+        
+         <hr>
+         <a class="btn grey darken-2" target="_blank" href="https://youtube.com/${channel.snippet.customUrl}">Visit Channel</a>
+
+         `;
+
+         showChannelData(output);
+
+        const playlistId =channel.contentDetails.relatedPlaylist.uploads;
+        requestVideoPlaylist(playlistId);
+
+
+
+     }).catch(err => alert("No channel by that name"));
  }
+
+function numWithCommas (x){
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function requestVideoPlaylist(playlistId){
+    const requestOptions = {
+        playlistId: playlistId,
+        part: "snippet",
+        maxResults: 10
+    }
+
+    const request = gapi.client.youtube.playlistItems.list(requestOptions)
+
+    request.execute(response =>{
+        console.log(response);
+        const playListItems  = response.result.items;
+        if(playListItems){
+            let output= '<br><h4 class ="center-align">Lates Videos</h4>'
+
+            //Looping through videos
+            playListItems.forEach(item => {
+                const videoId = item.snippet.resourceId.videoId;
+
+                output += `
+                    <div class="col s3">
+                    <iframe width="100%" height="auto' src="https://www.youtube.com/embed/${videoId}"
+                    frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    </div>
+                `;
+
+            });
+
+            //outputa videos
+            videoContainer.innerHTML = output;
+            
+        }else {
+            videoContainer.innerHTML = "No uploaded Videos";
+        }
+    });
+}
